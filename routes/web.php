7866a088289bb0e183d10bd11dd259b4a01ea8c1
Teresa Guarnizo <trsguarnizo@gmail.com>
2025-04-08 20:54:28 -0500

    corregido vista principal

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('turnos.index');
+    return redirect('/registros');
 });
 
 Route::get('/turnos', [TurnosController::class, 'index'])->name('turnos.index');
